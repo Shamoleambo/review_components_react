6938b65fa5ffdc9037c74f6309258f2a5932ebef
Tiago Gomes <tidgomes@gmail.com>
2026-03-02 14:35:33 -0300

    accessing content dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((coreConcept) => (
+              <CoreConcept {...coreConcept} key={coreConcept.title} />
+            ))}
           </ul>
         </section>
         <section id="examples">
